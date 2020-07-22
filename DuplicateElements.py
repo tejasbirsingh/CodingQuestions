5e67624f0d9b6668a674300a25429c6b088a636d
Tejas Bir Singh <tejasbir.rekhi@gmail.com>
2020-07-22 23:07:23 +0530

    Add files via upload

---
+++ DuplicateElements.py
@@ -0,0 +1,20 @@
+
+
+def duplicate(arr,n):
+
+	# Iterate through the array
+	for i in range(n):
+		# arr[i] will give the element at and take its absolute value 
+		# arr[value of arr[i] -1 ] -1 is done to make the index as 0 based 
+		# if the value is positive then mark it as negative 
+		if arr[abs(arr[i])-1] >=0 :
+			arr[abs(arr[i])-1] = -arr[abs(arr[i])-1]
+		#  If the value is already negative then print that element as it is the repeating element
+		else:
+			print(arr[i])
+
+
+
+a = [1,3,4,2,1,2,5,6,7,8,3]
+n =len(a)
+duplicate(a,n)
